593946f1636f68152d9716262ba1d560dfb7e3b8
guillermo <gmorenoc@uni.pe>
2022-07-27 22:20:22 -0500

    bp

--- Dataset_utils/Custom_Transforms.py
+++ Dataset_utils/Custom_Transforms.py
@@ -2,15 +2,17 @@
 
 class Tuple_to_dict(object):
 
-    def __init__(self,mode):
+    def __init__(self):
         self.new_sample={}
-        self.mode=mode
+        self.flag=0
 
     def __call__(self, sample):
         
-        if self.mode=="sample":
+        if self.flag==0:
             self.new_sample["x"]=sample
-        elif self.mode=="target":
-            self.new_sample["t"]=sample
+            self.flag=self.flag+1
+        else:
+            self.new_sample["y"]=sample
+            self.flag=0
 
         return self.new_sample
