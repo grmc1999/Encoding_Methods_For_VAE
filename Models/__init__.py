092251eb5421e46abac0096d1f2619144381da7c
guillermo <gmorenoc@uni.pe>
2022-07-28 21:43:10 -0500

    bp

--- Models/__init__.py
+++ Models/__init__.py
@@ -4,6 +4,6 @@ from .DL_utils import utils
 from DL_utils.utils import NeuralNet
 
 #import CVAE_NETs
-#import VAE_NETs
-#from . import pyro_CVAE,pyro_VAE,pytorch_VAE
+import VAE_NETs
+from . import pyro_CVAE,pyro_VAE,pytorch_VAE
 from . import CNN_graph
