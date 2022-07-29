50430889eea3a2dc3b3ebfcbd9456a0e2662a041
guillermo <gmorenoc@uni.pe>
2022-07-29 12:49:17 -0500

    bp

--- Train_utils/plot_utils.py
+++ Train_utils/plot_utils.py
@@ -7,6 +7,9 @@ class plot_training_sample(object):
         self.images_idxs=images_idxs
         self.titles=titles
         self.image_save_dir=image_save_dir
+        if "generated_imgs" not in os.listdir(self.image_save_dir):
+            os.mkdir(os.path.join(self.image_save_dir,"generated_imgs"))
+        self.image_save_dir=os.path.join(self.image_save_dir,"generated_imgs")
 
         #plt.ion()
         self.figure,self.axs=plt.subplots(1,len(self.images_idxs),figsize=(10,5),constrained_layout=True)
