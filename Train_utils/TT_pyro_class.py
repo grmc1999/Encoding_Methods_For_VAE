9cfb5fe96104604e33952905da59456f1176f756
guillermo <gmorenoc@uni.pe>
2022-07-28 22:38:01 -0500

    bp

--- Train_utils/TT_pyro_class.py
+++ Train_utils/TT_pyro_class.py
@@ -7,7 +7,7 @@ import copy
 import matplotlib.pyplot as plt
 import gc
 
-from .plot_utils import plot_training_sample
+from plot_utils import plot_training_sample
 
 class trainer(object):
     def __init__(self,model,dataset,epochs,folds,batch_size,use_cuda,data_dir,in_device=None,num_workers=0,args=["PhantomRGB"],uniform=True,view_out_state=True,MNIST_debug=False):
