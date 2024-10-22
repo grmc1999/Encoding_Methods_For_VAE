4a324e2d9e849bfc1a37aa849d2ff2510d3e0ad0
guillermo <gmorenoc@uni.pe>
2024-10-22 17:51:55 -0300

    exps and archs

--- Models/DL_utils/flatten_utils.py
+++ Models/DL_utils/flatten_utils.py
@@ -18,6 +18,10 @@ class hyb_view(object):
     def __init__(self,original_shape,cf,enc,dec,enc_dim_func,dec_dim_func):
         self.enc_dims=enc_dim_func(enc.im_layers[-1])
         self.dec_dims=dec_dim_func(dec.im_layers[0])
+        if len(original_shape)==2:
+            self.channel_shape=1
+        else:
+            self.channel_shape=original_shape[-3]
         original_shape[-1]=int(original_shape[-1]*cf)
         original_shape[-2]=int(original_shape[-2]*cf)
         self.i_shape=original_shape
@@ -28,6 +32,10 @@ class hyb_view(object):
             #out=x.view(x.shape[0],-1)
             out = rearrange(x,"b c h w -> b (c h w)")
         elif len(x.shape)==2: # batch layer_dim
-            out=rearrange(x,"b (c h w) -> b c h w",c=self.dec_dims,h=self.i_shape[-2],w=self.i_shape[-1])
+            out=rearrange(x,"b (c h w) -> b c h w",
+                          c=self.dec_dims,
+                          h=self.i_shape[-2]*(self.dec_dims//self.channel_shape),
+                          w=self.i_shape[-1]*(self.dec_dims//self.channel_shape)
+                          )
             #out=x.view([x.shape[0]]+self.i_shape)
         return out
